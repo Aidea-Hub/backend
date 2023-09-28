80f5da3e0b01e206f3a89585f108529af5ffad17
Marcus Tang <txk.marcus@gmail.com>
2023-09-28 15:25:19 +0800

    Update generateFunction

--- functions/src/generateIdeaFunctions.ts
+++ functions/src/generateIdeaFunctions.ts
@@ -3,19 +3,153 @@ import admin from "firebase-admin";
 import { cors } from "./config";
 import keywordExtractor from "keyword-extractor";
 import natural from "natural";
+import axios from "axios";
 
 const COST_TO_GENERATE_IDEA = 10;
 
-const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac urna ut nisl volutpat venenatis. Duis dapibus interdum turpis, non egestas nibh tincidunt in. Quisque eu orci enim. Morbi a consectetur urna. Nam bibendum, nulla eu malesuada pellentesque, diam urna sagittis sapien, sed tincidunt tellus enim at ligula. Praesent sollicitudin quam est. Vivamus ut purus at ligula pulvinar maximus. Nulla tempor sed ligula non rutrum.
+const API_REFLECTION_URL = process.env.API_GENERATE_REFLECTION || "";
+const API_RESEARCH_URL = process.env.API_GENERATE_RESEARCH || "";
 
-Sed quis placerat orci. Phasellus justo diam, condimentum at porta at, auctor vitae dui. In in erat eu tellus convallis sagittis ut et nulla. Etiam a fringilla lorem. Mauris cursus maximus metus sed bibendum. Praesent auctor est eget velit commodo malesuada. Pellentesque tincidunt suscipit ipsum, eget congue justo consectetur sit amet. Fusce dictum vulputate sapien eget ultrices. Vivamus eleifend viverra tellus vitae rhoncus. Proin imperdiet dapibus mauris id consequat.
+const generateReflection = async (
+  title: string,
+  description: string,
+  prompt: string,
+  format: string
+) => {
+  try {
+    const response = await axios.post(API_REFLECTION_URL, {
+      prompt,
+      format,
+      title,
+      description,
+    });
+    return response.data.response;
+  } catch (error) {
+    console.error("Error making API call:", error);
+    return "Failed to generate response, please contact us on Discord for assistance.";
+  }
+};
 
-Nunc magna nisl, rutrum ut elit in, pellentesque pellentesque erat. Nulla facilisi. Curabitur et orci vitae turpis malesuada ornare. Duis mattis metus libero, ut tempus nisi lobortis in. Fusce a hendrerit sem. Quisque eleifend pulvinar mauris eget scelerisque. Maecenas consectetur, massa eu aliquam mollis, justo quam auctor ipsum, nec ultricies elit enim quis nunc. Nullam lobortis faucibus nisl vitae varius. Quisque imperdiet ligula eget sapien tincidunt, sit amet congue ligula tristique. Donec eu commodo ipsum, vel commodo nibh. Sed accumsan libero non turpis sodales ullamcorper. Nam eu lectus placerat, consectetur nisi quis, consectetur mi.
+const generateResearch = async (description: string) => {
+  try {
+    const response = await axios.post(API_RESEARCH_URL, {
+      description,
+    });
+    return response.data.response;
+  } catch (error) {
+    console.error("Error making API call:", error);
+    return "Failed to generate response, please contact us on Discord for assistance.";
+  }
+};
+
+const productCapabilitiesPrompt =
+  "For the following idea, suggest 10 of the most useful and innovative product capabilities. Avoid the common pitfall of failing to consider how the product serves the problem in the process. Consider if the product have the capabilities to succeed in the chosen problem space.";
+const productCapabilitiesFormat = `Product Capabilities: <List of product capabilities, elaborating on each product capability>
+Conclusion: <Conclusion of product capabilities>
 `;
 
+const moatPrompt =
+  "For the given product idea, identify 2 to 3 potential moats or sustainable competitive advantages. These moats should protect its market position, deter new entrants, and prevent existing competitors from replicating its success. Example: One of Apple's moat is its integrated ecosystem of products. Apple's products, from iPhone, Macs, and services like iCloud, are tightly integrated into a single ecosystem, creating a seamless user experience that is difficult for competitors to match. This tight integration also makes it difficult for consumers to switch out of Apple.";
+const moatFormat = `Moat: <List of moats, elaborating on each moat>
+Conclusion: <Conclusion of moats>
+`;
+
+const productLifecyclePrompt =
+  "For the given product idea it is in the introduction stage. Outline its journey through the product life cycle. Highlight challenges and strategies for each stage and how to achieve product-market fit during growth. Emphasize user acquisition tactics in the introduction and their evolution in later stages.";
+const productLifecycleFormat = `Introduction Stage:
+- Steps: <Steps for introduction>
+- Challenges: <Challenges and tactics for user acquisition>
+- Strategies: <Strategies for engagement>
+
+Growth Stage:
+- Steps: <Steps for growth>
+- Product-Market Fit: <Strategies for fit>
+- Evolution of Tactics: <Changes in user acquisition tactics>
+
+Maturity Stage:
+- Steps: <Steps for maturity>
+- Challenges: <Challenges faced>
+- Strategies: <Strategies to maintain user base>
+
+Decline Stage:
+- Steps: <Steps for decline>
+- Challenges: <Challenges faced>
+- Strategies: <Revitalization strategies>
+
+Conclusion:
+- Summary: <Brief overview of the product's lifecycle journey>
+`;
+
+const businessModelPrompt =
+  "For the following idea, outline a monetization and pricing strategy considering costs, value, and competition. Emphasize the suitability of the strategy for the product's stage, target users, and problem space. Discuss potential revenue streams and factors influencing pricing decisions.";
+const businessModelFormat = `Cost-Based Pricing:
+- Production Costs: <Costs associated with producing and maintaining the product>
+- Desired Profit Margin: <Targeted profit margin>
+
+Value-Based Pricing:
+- User Benefits: <Benefits the product provides>
+- Perceived Value: <Value perceived by users>
+
+Competition-Based Pricing:
+- Competitor Pricing: <How competitors price similar products>
+- Product Comparison: <Differences and unique selling points of your product vs. competition>
+
+Monetization Strategy:
+- Strategy Details: <Details of the chosen monetization method, e.g., subscription, one-time payment>
+- Revenue Streams: <Possible revenue streams for the product>
+- Currency of Success: <If not monetary, other metrics of success, e.g., volume of messages>
+
+Conclusion:
+- Strategy Justification: <Why the chosen pricing strategy is suitable for target users and problem space>
+- Influencing Factors: <Factors that influenced the pricing decisions, such as production costs, perceived value, competition>
+`;
+
+const brandingPrompt =
+  "For the following idea, define a compelling brand identity by suggesting a product name and designing a logo. Consider factors like name length, reproducibility, domain availability, cultural references, and potential resemblances. Discuss the rationale behind the name, potential alternatives, and the significance of the chosen logo.";
+const brandingFormat = `Product Name:
+- Suggested Name: <Proposed product name>
+- Meaning Behind the Name: <Rationale or story behind the name>
+- Alternatives Considered: <Other names that were thought of>
+- Factors Considered: <Aspects like domain availability, cultural implications, and more>
+
+Logo Design:
+- Logo Description: <Brief description of the logo's design>
+- Significance: <What the logo represents and its connection to the product>
+- Design Challenges: <Any challenges faced during the design process and how they were addressed>
+- Inspirations: <Any sources of inspiration, such as Pinterest, Dribbble, etc.>
+
+Conclusion:
+- Brand Identity Justification: <Why the chosen name and logo best represent the product's identity>
+- Potential Impact: <How this branding might influence the product's perception and recognition in the market>
+`;
+
+const uiuxPrompt =
+  "Given a product's details, define both the user experience (UX) and user interface (UI). Describe key workflows for optimal user interaction and the rationale behind their design. Highlight UI considerations tailored to specific features and functionalities. Reflect on transparency, ethics, privacy, and user empowerment, especially in decision-making features. Discuss design prototypes and the final design choices.";
+const uiuxFormat = `Sample Key Workflows:
+- Workflow 1: <Description and reasoning>
+- Workflow 2: <Description and reasoning>
+- Workflow 3: <Description and reasoning>
+
+User Experience Considerations:
+- Branding Synergy: <How UX decisions resonate with the brand>
+- User Emotions & Connections: <How the UX design fosters certain feelings or memories>
+- Feedback & Iteration: <Feedback sources and changes made based on feedback>
+
+User Interface Considerations:
+- Transparency & Trust: <How the UI communicates the capabilities and limitations of features>
+- Ethical Considerations: <Design choices made to ensure ethical interactions>
+- Privacy & Data Collection: <How the UI informs users about data usage and collection>
+- User Empowerment: <UI elements that give users control over automated actions or recommendations>
+
+Conclusion:
+- Overall Strategy: <Summary of the holistic design strategy for both UX and UI>
+`;
 // To be called once the idea's title and description is decided
-export const generateIdeaContent = functions.https.onRequest(
-  async (req, res) => {
+export const generateIdeaContent = functions
+  .runWith({
+    timeoutSeconds: 300,
+  })
+  .https.onRequest(async (req, res) => {
     cors(req, res, async () => {
       const data = req.body;
       const userId = data.userId;
@@ -47,7 +181,7 @@ export const generateIdeaContent = functions.https.onRequest(
 
       // Decrement user credits by 10
       await userRef.update({
-        credits: admin.firestore.FieldValue.increment(COST_TO_GENERATE_IDEA),
+        credits: admin.firestore.FieldValue.increment(-COST_TO_GENERATE_IDEA),
       });
       const url = await generateImageUrl(title);
       console.log(url);
@@ -65,13 +199,42 @@ export const generateIdeaContent = functions.https.onRequest(
 
       const ideaId = ideaRef.id;
 
-      // TODO replace with actual api/fn calls to generate the respective content, can generate in parallel
-      const productCapabilities = LOREM_IPSUM;
-      const competitiveLandscapeAndMoat = LOREM_IPSUM;
-      const productLifecycle = LOREM_IPSUM;
-      const businessModel = LOREM_IPSUM;
-      const branding = LOREM_IPSUM;
-      const uiux = LOREM_IPSUM;
+      const results = await Promise.all([
+        generateReflection(
+          title,
+          description,
+          productCapabilitiesPrompt,
+          productCapabilitiesFormat
+        ),
+        generateResearch(description),
+        generateReflection(title, description, moatPrompt, moatFormat),
+        generateReflection(
+          title,
+          description,
+          productLifecyclePrompt,
+          productLifecycleFormat
+        ),
+        generateReflection(
+          title,
+          description,
+          businessModelPrompt,
+          businessModelFormat
+        ),
+        generateReflection(title, description, brandingPrompt, brandingFormat),
+        generateReflection(title, description, uiuxPrompt, uiuxFormat),
+      ]);
+
+      console.log("Completed generation of results:", results);
+
+      const [
+        productCapabilities,
+        competitiveLandscape,
+        moat,
+        productLifecycle,
+        businessModel,
+        branding,
+        uiux,
+      ] = results;
 
       // Create a new content document in the "idea_contents" collection
       const ideaContentRef = admin
@@ -81,7 +244,8 @@ export const generateIdeaContent = functions.https.onRequest(
       await ideaContentRef.set({
         idea_id: ideaId,
         productCapabilities: productCapabilities,
-        competitiveLandscapeAndMoat: competitiveLandscapeAndMoat,
+        competitiveLandscape: competitiveLandscape,
+        moat: moat,
         productLifecycle: productLifecycle,
         businessModel: businessModel,
         branding: branding,
@@ -92,8 +256,7 @@ export const generateIdeaContent = functions.https.onRequest(
         message: `Idea ${ideaId} and its content were successfully created`,
       });
     });
-  }
-);
+  });
 
 const generateImageUrl = async (title: string): Promise<string> => {
   const extractedKeywords = keywordExtractor.extract(title, {
