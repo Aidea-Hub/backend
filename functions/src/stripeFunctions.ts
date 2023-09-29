6c34ddc348f6583dc2c7ca1c9720761c660236b1
Marcus Tang <txk.marcus@gmail.com>
2023-09-29 21:02:07 +0800

    Fix linting errors for stripeFunctions.ts

--- functions/src/stripeFunctions.ts
+++ functions/src/stripeFunctions.ts
@@ -1,42 +1,45 @@
 import * as functions from "firebase-functions";
+import Stripe from "stripe";
 import { cors } from "./config";
 
+const YOUR_DOMAIN = "https://aidea-hub.netlify.app/";
+// const YOUR_DOMAIN = "http://localhost:3000/";
+const STRIPE_KEY = process.env.STRIPE_KEY || "";
+const stripe = new Stripe(STRIPE_KEY, {
+  apiVersion: "2023-08-16",
+  typescript: true,
+});
 
-const YOUR_DOMAIN = 'https://aidea-hub.netlify.app/';
-// const YOUR_DOMAIN = 'http://localhost:3000/';
-const stripe_key = process.env.STRIPE_KEY || "";
-const stripe = require('stripe')(stripe_key);
-
-export const createCheckoutSession = functions.https.onRequest(async (req, res) => {
-  cors(req, res, async () => {
-    // const S = new Stripe('sk_test_51NvIRjFMQoAOlyFr1sf5sVtTPrWnsZpUGnSX12IwNnsCIPJrlCELrFYKrUjjplOVYSCCwlSMUAaMC8paJaG4CuRO00w9a8a3r2');
-    const session = await stripe.checkout.sessions.create({
+export const createCheckoutSession = functions.https.onRequest(
+  async (req, res) => {
+    cors(req, res, async () => {
+      const session = await stripe.checkout.sessions.create({
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: 'price_1NvNLBFMQoAOlyFrTKBp5ES5',
+            price: "price_1NvNLBFMQoAOlyFrTKBp5ES5",
             quantity: 1,
           },
         ],
-        mode: 'subscription',
-        success_url: `${YOUR_DOMAIN}` + 'success',
-        cancel_url: `${YOUR_DOMAIN}` + 'fail',
-        automatic_tax: {enabled: true},
+        mode: "subscription",
+        success_url: `${YOUR_DOMAIN}` + "success",
+        cancel_url: `${YOUR_DOMAIN}` + "fail",
+        automatic_tax: { enabled: true },
       });
-    
-      res.redirect(303, session.url);
-    // res.status(200)
-  });
-});
 
+      res.redirect(303, session.url!);
+      // res.status(200)
+    });
+  }
+);
 
 // export const webhook = functions.https.onRequest(async (req, res) => {
 //   cors(req, res, async () => {
 //     console.log("ASDASDASD")
 //     const sig = req.headers['stripe-signature'];
-  
+
 //     let event;
-  
+
 //     try {
 //       event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
 //     } catch (err) {
@@ -56,10 +59,8 @@ export const createCheckoutSession = functions.https.onRequest(async (req, res) 
 //       default:
 //         console.log(`Unhandled event type ${event.type}`);
 //     }
-  
+
 //     // Return a 200 response to acknowledge receipt of the event
 //     res.send();
 //   });
 // });
-
-
